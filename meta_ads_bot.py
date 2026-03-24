d46936d8b85eba680aed3ab183ac83748c82a224
huyndt12-source <huyndt12@gmail.com>
2026-03-24 16:14:50 +0700

    Update meta_ads_bot.py

--- meta_ads_bot.py
+++ meta_ads_bot.py
@@ -6,12 +6,10 @@ BOT_TOKEN = "8603101291:AAGYsIGfCLGcqfby3oUk88ILOFRWMo8X2S4"
 CHAT_ID = "2077738684"
 AD_ACCOUNT_ID = "act_3635946859955819"
 META_TOKEN = os.getenv("META_TOKEN")
-
-def send_telegram(text):
+OFFSET = 0
+def send_telegram(chat_id, text):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    payload = {"chat_id": CHAT_ID, "text": text, "parse_mode": "HTML"}
-    resp = requests.get(url, params=payload)
-    print(f"Telegram: {resp.json()}")
+    payload
 
 def main():
     url = f"https://graph.facebook.com/v20.0/{AD_ACCOUNT_ID}/insights"
